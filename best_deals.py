5446c41c8815842ab091eab108048ea7d9c5c138
Daniel Paradis <dan.paradis@maine.edu>
2018-03-01 21:02:20 -0500

    Fixed bugs and added larry allen

--- best_deals.py
+++ best_deals.py
@@ -25,11 +25,16 @@ for player in sets:
 	card_urls = [line.rstrip('\n') for line in open('sets/' + player)]; # get list of all muthead urls for player
 
 	for url in card_urls:
+		string = ['none'];
 		page = requests.get(url); # get page
 		tree = html.fromstring(page.content); # load source code
 		string = tree.xpath('//div[@class="player-prices-live-auctions"]/table/tr[2]/td[4]/text()');
+		nodata = 0;
 
-		price = int(string[0].translate(None, ',')); # remove comma from price and convert to an int
+		if not string:
+			nodata = 1;
+		else:
+			price = int(string[0].translate(None, ',')); # remove comma from price and convert to an int
 
 		if first == 1: # if it is the first url, it's the reward
 			reward_price = price;
@@ -41,8 +46,10 @@ for player in sets:
 	post_tax_price = int(float(reward_price) * 0.9);
 	profit = int(post_tax_price) - int(sum_of_parts);
 
-	if profit > 0:
+	if profit > 0 and nodata != 1:
 		print bcolors.OKGREEN + str(player) + ':\t\t' + str(reward_price) + '\t\t' + str(post_tax_price)  +'\t\t\t' + str(sum_of_parts) + '\t\t' + str(profit) + bcolors.ENDC;
+	if nodata == 1:
+		print "Incomplete data";
 #	else:
 #		print bcolors.FAIL + str(player) + ':\t\t' + str(reward_price) + '\t\r' + str(post_tax_price)  +'\t\t\t' + str(sum_of_parts) + '\t\t' + str(profit) + bcolors.ENDC;
 	
