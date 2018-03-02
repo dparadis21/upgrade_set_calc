ad4f5800c535a98be600e26d3da59578390858bb
Daniel Paradis <dan.paradis@maine.edu>
2018-03-01 20:25:29 -0500

    Base functionality

--- best_deals.py
+++ best_deals.py
@@ -1,9 +1,46 @@
 from lxml import html
 import requests
+import os
+import io
 
-page = requests.get ('https://www.muthead.com/18/players/prices/31010-nick-foles/xbox-one')
-tree = html.fromstring(page.content)
+path = 'sets'
+sets = os.listdir(path)
 
-buyitnow = tree.xpath('//div[@class="player-prices-live-auctions"]/table/tr[*]/td[4]/text()')
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-print 'Buy it now Price:  ', buyitnow
+for player in sets:
+	reward_price = 0;
+	sum_of_parts = 0;
+	first = 1;
+
+	card_urls = [line.rstrip('\n') for line in open('sets/' + player)]; # get list of all muthead urls for player
+
+	for url in card_urls:
+		page = requests.get(url); # get page
+		tree = html.fromstring(page.content); # load source code
+		string = tree.xpath('//div[@class="player-prices-live-auctions"]/table/tr[2]/td[4]/text()');
+
+		price = int(string[0].translate(None, ',')); # remove comma from price and convert to an int
+
+		if first == 1: # if it is the first url, it's the reward
+			reward_price = price;
+		else:
+			sum_of_parts += price;
+
+		first = 0;
+
+	post_tax_price = int(float(reward_price) * 0.9);
+
+	if int(reward_price) > int(post_tax_price):
+		print bcolors.OKGREEN + str(player) + ': ' + str(reward_price) + '\t' + str(post_tax_price)  +'\t' + str(sum_of_parts) + bcolors.ENDC;
+	else:
+		print bcolors.WARNING + str(player) + ': ' + str(reward_price) + '\t' + str(post_tax_price)  +'\t' + str(sum_of_parts) + bcolors.ENDC;
+	
