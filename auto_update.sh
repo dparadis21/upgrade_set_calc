24f7641b514fd8be0061a6bafaec317ae09c4f03
Daniel Paradis <dan.paradis@maine.edu>
2018-03-03 02:42:48 -0500

    Fixed stuff

--- auto_update.sh
+++ auto_update.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 python /home/dan/Documents/upgrade_set_calc/update.py > /home/dan/Documents/upgrade_set_calc/tmp
 cat /home/dan/Documents/upgrade_set_calc/tmp > /home/dan/Documents/upgrade_set_calc/current_prices
 rm /home/dan/Documents/upgrade_set_calc/tmp
