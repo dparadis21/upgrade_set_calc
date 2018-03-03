0c5e673805acc483399eba01991e84b8504237f6
Daniel Paradis <dan.paradis@maine.edu>
2018-03-03 02:37:28 -0500

    Auto update

---
+++ auto_update.sh
@@ -0,0 +1,3 @@
+python /home/dan/Documents/upgrade_set_calc/update.py > /home/dan/Documents/upgrade_set_calc/tmp
+cat /home/dan/Documents/upgrade_set_calc/tmp > /home/dan/Documents/upgrade_set_calc/current_prices
+rm /home/dan/Documents/upgrade_set_calc/tmp
